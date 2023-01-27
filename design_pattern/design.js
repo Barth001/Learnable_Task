dad5d5e45537e19d1b36a026ae56b597eb46c8e0
Millimeter-Wave <alaekwebartholomew@gmail.com>
2023-01-27 09:54:51 -0800

    subject updated

--- design_pattern/design.js
+++ design_pattern/design.js
@@ -1,46 +1,22 @@
 class Telephone{
     constructor(){
-        this._dataBase = Set()
+        this.dataBase = []
     }
 
     addPhoneNumber(phoneNumber){
-        this._dataBase.push(phoneNumber)
+        this.dataBase.push(phoneNumber)
     }
 
     removePhoneNumber(phoneNumber){
-        this._dataBase = this._dataBase.filter(num => num !== phoneNumber)
+        this.dataBase = this.dataBase.filter(num => num !== phoneNumber)
     }
 
     dialPhoneNumber(phoneNumber){
-        let isAvailable = this._dataBase.includes(phoneNumber);
+        let isAvailable = this.dataBase.includes(phoneNumber);
         if (isAvailable)
         console.log(`Now Dialling ${phoneNumber}`);
-    }
-
-    notifyObserver(args){
-        this._observers.forEach(observer => {
-            observer(args)
-        });
     }
     
 }
 
 // Observer class
-class Observer extends Telephone{
-    constructor(){
-        super()
-        this._observers = []
-    }
-
-    addObserver(observer){
-        this._observers.push(observer);
-    }
-
-    removeObserver(observer){
-        this._observers = this._observers.filter(obs => obs !== observer);
-    }
-
-    notify(data){
-        this._observers.forEach((observer) => observer.notify(data))
-    }
-}
